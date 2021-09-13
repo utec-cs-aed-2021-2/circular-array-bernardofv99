306e14ca3b2c3926a9bf8e4404bd4e10a49a2cb6
bernardofv99 <bernardo.flores@energyatech.com>
2021-09-12 22:51:46 -0500

    update

--- circulararray.h
+++ circulararray.h
@@ -11,21 +11,70 @@ private:
     
 public:
     CircularArray();
+
     CircularArray(int _capacity);
+    
     virtual ~CircularArray();
+    
     void push_front(T data);
+    
     void push_back(T data);
+    
     void insert(T data, int pos);
+    
     T pop_front();
+
     T pop_back();
-    bool is_full();
-    bool is_empty();
-    int size();
+
+    bool is_full(){
+        return next(back) == front;
+    }
+    
+    bool is_empty(){
+    int flag = 0;
+    int i;
+    for (i = 0; i < capacity; i++) {
+        if (array[i] != 0)
+            {
+                flag ++;
+            }
+        else{
+            continue;
+        }
+    }
+    if (flag == 0){
+        return false;
+    }
+    else {
+        return true;
+    } 
+    }
+    
+    int size(){
+        if (front == back){
+            return 1;
+        }
+        else if (front > back){
+            return capacity - (front - back) - 1;
+        } 
+        else if (back > front){
+            return (front - back) + 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
     void clear();
+    
     T &operator[](int);
+    
     void sort();
+    
     bool is_sorted();
+    
     void reverse();
+    
     string to_string(string sep=" ");
 
 private:
